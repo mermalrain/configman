beab6674730a37bc5b9d6d868df8b3a2f48d1744
Jobs Wang <wchswchs@gmail.com>
2015-06-11 15:12:55 +0800

    修改add_config example

--- example/add_config.php
+++ example/add_config.php
@@ -1,5 +1,6 @@
 <?php
-require '../api/BaseServiceConfig.class.php';
+define('ROOT_PATH', __DIR__ . '/..');
+require_once(ROOT_PATH . '/Autoloader.class.php');
 
 use Configman\Api\BaseServiceConfig;
 
